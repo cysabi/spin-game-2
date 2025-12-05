9f4f2976da41ad5c1c4be8092d91c6cc35db5037
hfellerhoff <henryfellerhoff@gmail.com>
2025-12-05 16:50:02 -0500

    power is angular momentum

--- src/main.ts
+++ src/main.ts
@@ -167,7 +167,7 @@ function bowlGravity(spinner: Body, dt: number) {
   spinner.applyLinearImpulse(
     Vec2.mul(
       angle,
-      (Math.max(0, angle.lengthSquared() - 5) / 2) * (dt / 100000),
+      (Math.max(0, angle.lengthSquared() - 5) / 3) * (dt / 100000),
     ),
     spinner.getPosition(),
   );
@@ -224,7 +224,7 @@ function update(dt: number) {
     }
   } else {
     const dt_seconds = dt / 1000;
-    const SPEED = 50;
+    const SPEED = 200;
 
     // const testStepDelta = 20;
     // const testStepResolution = 64;
@@ -284,13 +284,8 @@ function update(dt: number) {
       player2arrow.setAngle(newAngle);
     }
 
-    const player1power =
-      Math.abs(player1spinner.getAngularVelocity()) +
-      player1spinner.getLinearVelocity().length();
-
-    const player2power =
-      Math.abs(player2spinner.getAngularVelocity()) +
-      player2spinner.getLinearVelocity().length();
+    const player1power = Math.abs(player1spinner.getAngularVelocity());
+    const player2power = Math.abs(player2spinner.getAngularVelocity());
 
     if (hasPlayer1Launched && hasPlayer2Launched) {
       if (player1power < 10) {
